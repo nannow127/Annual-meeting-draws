4f1f3390c37b63a906e17767ed2447469d2ddb34
Nanno127 <nannow127@gmail.com>
2023-12-14 20:57:12 +0800

    穿件学生界面

---
+++ src/components/data.js
@@ -0,0 +1,66 @@
+export const studentList = [
+  {
+    emil: '1',
+    name: '',
+    englishName: '张三',
+    grade: 'G1',
+    form: '1B',
+    house: 'A'
+  },
+
+  {
+    emil: '2',
+    name: '',
+    englishName: '张三',
+    grade: 'G2',
+    form: '2B',
+    house: 'A'
+  },
+  {
+    emil: '3',
+    name: '',
+    englishName: '王五',
+    grade: 'G2',
+    form: '1C',
+    house: 'N'
+  },
+  {
+    emil: '4',
+    name: '',
+    englishName: '家',
+    grade: 'G1',
+    form: '1A',
+    house: 'B'
+  },
+  {
+    emil: '5',
+    name: '',
+    englishName: '医',
+    grade: 'G3',
+    form: '1C',
+    house: 'D'
+  },
+  {
+    emil: '6',
+    name: '',
+    englishName: '丙丁',
+    grade: 'G1',
+    form: '1A',
+    house: 'C'
+  }
+]; // 学生数据
+
+export const gradeList = [
+  {
+    value: 'G1',
+    label: 'G1'
+  },
+  {
+    value: 'G2',
+    label: 'G2'
+  },
+  {
+    value: 'G3',
+    label: 'G3'
+  }
+];
